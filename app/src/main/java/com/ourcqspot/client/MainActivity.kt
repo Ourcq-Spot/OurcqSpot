6bce7f3a24b8feea41c1488863a633f3a294651a
Adam Sansone <adamalessioscoldirect@gmail.com>
2025-01-19 00:29:58 +0100

    v0.0.1 (safe device status / navigation bars)

--- app/src/main/java/com/ourcqspot/client/MainActivity.kt
+++ app/src/main/java/com/ourcqspot/client/MainActivity.kt
@@ -3,8 +3,10 @@ package com.ourcqspot.client
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import androidx.navigation.compose.rememberNavController
 import com.ourcqspot.client.graphs.HomeNavGraph
 import com.ourcqspot.client.graphs.RootNavGraph
@@ -13,6 +15,10 @@ import com.ourcqspot.client.ui.theme.Test_NestedBottomNavBarTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        /* WindowCompat.getInsetsController(window, window.decorView)
+            .isAppearanceLightStatusBars = false */
         setContent {
             Test_NestedBottomNavBarTheme {
                 RootNavGraph(navController = rememberNavController())
--- app/src/main/java/com/ourcqspot/client/screens/home/HomeScreen.kt
+++ app/src/main/java/com/ourcqspot/client/screens/home/HomeScreen.kt
@@ -1,14 +1,20 @@
 package com.ourcqspot.client.screens.home
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.shape.RoundedCornerShape
 //noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.material.BottomAppBar
@@ -28,6 +34,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hierarchy
@@ -36,6 +44,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.ourcqspot.client.BottomBarScreen
+import com.ourcqspot.client.R
 import com.ourcqspot.client.graphs.HomeNavGraph
 import com.ourcqspot.client.ui.theme.SelectedBottomItemColor
 import com.ourcqspot.client.ui.theme.UnselectedBottomItemColor
@@ -49,10 +58,12 @@ fun HomeScreen(navController: NavHostController = rememberNavController()) {
         },
         bottomBar = {
             BottomBar(navController = navController)
-        }
+        },
+        //contentWindowInsets =
     ) { innerPadding ->
         Column (
             modifier = Modifier//.padding(innerPadding)
+                .navigationBarsPadding()
                 .padding(PaddingValues(top = innerPadding.calculateTopPadding()))
                 .fillMaxSize()
                 .background(color = Color.Yellow),
@@ -70,30 +81,24 @@ fun TopBar(navController: NavHostController) {
     val navBackStackEntry by navController.currentBackStackEntryAsState()
     val currentDestination = navBackStackEntry?.destination
 
-    Column {}
     androidx.compose.material.TopAppBar (
         modifier = Modifier
-            .fillMaxWidth()
-            .defaultMinSize(minHeight = 75.dp),
+            .statusBarsPadding()
+            //.defaultMinSize(minHeight = 75.dp)
+            .fillMaxWidth(),
+        backgroundColor = Color(0xffff00ff)
         //contentColor = Color(0xff0E2176)
-        backgroundColor = Color(0xffffffff)
     ) {
-        val bottomBarDestination = screens.any { it.route == currentDestination?.route }
-        if (bottomBarDestination) {
-            BottomNavigation (
-                //modifier = Modifier//.size(width = 300.dp/*, height = 100.dp*/)
-                //.width(width = 300.dp),
-                backgroundColor = Color.Transparent,
-                elevation = 0.dp
-            ) {
-                screens.forEach { screen ->
-                    AddItem(
-                        screen = screen,
-                        currentDestination = currentDestination,
-                        navController = navController,
-                    )
-                }
-            }
+        Row (
+            modifier = Modifier.background( color = Color(0xffff0000) )
+                .defaultMinSize(minHeight = 30.dp, minWidth = 10.dp)
+                //.fillMaxSize()
+        ) {
+            Image(
+                painter = painterResource(R.drawable.logo_ourcqspot),
+                contentDescription = stringResource(id = R.string.logo_ourcqspot_content_description),
+                //modifier = Modifier.height(height = 20.dp)
+            )
         }
     }
 }
@@ -106,6 +111,7 @@ fun BottomBar(navController: NavHostController) {
 
     BottomAppBar (
         modifier = Modifier
+            .navigationBarsPadding()
             .padding(vertical = 30.dp, horizontal = 27.5.dp)
             .clip(RoundedCornerShape(20.dp))
             .defaultMinSize(minHeight = 75.dp),
