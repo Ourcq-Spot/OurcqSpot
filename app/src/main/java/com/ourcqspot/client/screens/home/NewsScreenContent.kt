2382b5332ce558229f0ab9b77a399e5e77a4ae55
Adam Sansone <adamalessioscoldirect@gmail.com>
2025-01-23 00:21:22 +0100

    v0.0.7 (exemple NewsScreenContent)

--- app/src/main/java/com/ourcqspot/client/screens/home/NewsScreenContent.kt
+++ app/src/main/java/com/ourcqspot/client/screens/home/NewsScreenContent.kt
@@ -1,9 +1,35 @@
 package com.ourcqspot.client.screens.home
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun NewsScreenContent() {
     // Fonctionnement identique à MapScreenContent.
     // Ce @Composable est intégré dans HomeNavGraph (dans NavHost {composable() { ... } })
+
+    Column ( // Tu peux changer Column en Row, en Box, en LazyColumn, ...etc
+        Modifier
+            .fillMaxSize()
+            .background(Color.Cyan) // À retirer
+    ) {
+        // -------------------
+        // À remplacer par le contenu
+        Box (
+            Modifier
+                .padding(15.dp)
+                .fillMaxSize()
+                .background(Color.Green)
+        ) {
+
+        }
+        // -------------------
+    }
 }
